3b10782e4f63c267d8fb3d1e097e223f7f93432c
gabriel <grigorjev.ilja@gmail.com>
2022-06-16 08:00:26 +0300

    Init

---
+++ src/test/java/com/ilgrig/study/BookStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilgrig.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
